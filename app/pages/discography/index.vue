d1d3d0dc2cb7c95d45559e4674ed598623c4c6b6
TakehiroTakahashi <taka.mc.0509@gmail.com>
2019-02-21 16:12:12 +0900

    add basic page

---
+++ app/pages/discography/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+    #discography
+        p {{ page_name }}
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+@Component
+export default class Discography extends Vue {
+    page_name: string = 'Discography Page'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ app/pages/link/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+    #link
+        p {{ page_name }}
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+@Component
+export default class Link extends Vue {
+    page_name: string = 'Link Page'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
